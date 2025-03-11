ff63c4aed5d7a5766ba3ccca9f9982eb148ad202
jdam <jdam5000@gmail.com>
2025-03-11 10:00:03 -0400

    Bugs fixed and the player now is stopped if is talking

--- Gratos-1/Maps/Warp/Global.gd
+++ Gratos-1/Maps/Warp/Global.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export_file("*.tscn") var arena 
+var talking = false
+var boss = false
 var reaady = false
 var battle= false
 var encounter : int = 30:
@@ -17,19 +19,19 @@ var player_last_position : Vector2 = Vector2(0,0)
 
 func _ready():
 	randomize()
-	encounter = 3#randi_range(25,50)
+	encounter = randi_range(25,50)
 	
 func change_to(tree, type: String):
 	
 	if type == "Map":
-		tree.get_node(type).queue_free()
+		tree.get_child(0).queue_free()
 		#current_tree.change_to_scene(arena)
 		var battle_scene = load(arena).instantiate()
 		battle = true
 		tree.add_child(battle_scene)
 		
 	elif type == "Combate":
-		tree.get_node(type).queue_free()
+		tree.get_child(0).queue_free()
 		#current_tree.change_to_scene(arena)
 		var battle_scene = load(GameControl.current_map).instantiate()
 		battle = false
